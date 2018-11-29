6a0f0936c54a569d43d39539c006c0395958e082
Tiago Mendes <tiagocmendes@ua.pt>
2018-11-29 23:18:55 +0000

    Added Players Complete Dataset

---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+
+    private static int id = 0;
+    private int myID;
+    private String name;
+    private int age;
+    private String country;
+    private int overall;
+    private int potential;
+    private String club;
+
+}
